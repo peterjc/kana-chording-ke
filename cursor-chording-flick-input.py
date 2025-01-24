6829887cc6549f43b707b0f597316a19c91405f7
Peter J. A. Cock <p.j.a.cock@googlemail.com>
2025-01-24 02:41:00 +0000

    Expand most (all?) single kana in romaji mode

--- cursor-chording-flick-input.py
+++ cursor-chording-flick-input.py
@@ -20,11 +20,79 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-"""Generate Karabina Elements JSON for flick-input like kana chording."""
+"""Generate Karabina Elements JSON for flick-input like kana chording.
 
+```
+あいうえお　アイウエオ	aiueo
+ぁぃぅぇぉ　ァィゥェォ　x+ or l+ for small vowels
+かきくけこ　カキクケコ	k+
+がぎぐげご　ガギグゲゴ	g+
+さしすせそ　サシスセソ	s+ - can use si or shi for し
+ざじずぜじ　ザジズゼゾ　z+ - can use zi or ji  for じ
+たちつてと　タチツテト　t+ - can use ti or chi for ち
+・・っ・・　・・ッ・・　did this with xtsu, xtu, xtsu or ltu
+だぢづでど　ダヂヅデド　d+
+なにぬねの　ナニヌネノ　n+
+はひふへほ　ハヒフヘホ　h+ - can use hu for fu for ふ
+ばびぶべぼ　バビブベボ　b+
+ぱぴぷぺぽ　パピプペポ　p+
+まみむめも　マミムメモ　m+
+や・ゆ・よ　ヤ・ユ・ヨ　y+
+ゃ・ゅ・ょ　ャ・ュ・ュ　xy+
+らりるれろ　ラリルレロ　r+
+わゐ・ゑを　ワヰ・ヱヲ　w+ - with wa/wyi/(vu)/wye/wo for the obsolete ones
+・・ゔ・・　・・ヴ・・　v+
+・・ん・・　・・ン・・　nn
+・・ー・・　・・ー・・　used minus - so no need to map this
+```
+
+"""
+
+rows = [
+    "x",  # small vowels - could use prefix l instead.
+    "k",
+    "g",
+    "s",
+    "z",
+    "t",
+    "xt",  # just for small tsu aka xtsu aka xtu etc.
+    "d",
+    "n",
+    "h",
+    "b",
+    "p",
+    "m",
+    "y",
+    "xy",  # small ya, yu, yo
+    "r",
+    "w",  # including ん in place of wu
+    "v",
+]
 vowels = "aiueo"  # used as suffix for default romaji mapping
 modifiers = [None, "left_arrow", "up_arrow", "right_arrow", "down_arrow"]
 assert len(vowels) == len(modifiers)
+exceptions = {
+    "si": "shi",  # use typical romaji for kana し although "si" works anyway
+    "zi": "ji",  # use typical romaji for kana じ although "zi" works anyway
+    "ti": "chi",  # use typical romaji for kana ち although "ti" works anyway
+    "xta": None,  # there is no small た
+    "xti": None,  # there is no small ち
+    "xtu": "xtsu",  # use more logical code for small っ
+    "xte": None,  # there is no small て
+    "xto": None,  # there is no small と
+    "hu": "fu",  # use typical romaji for kana ふ although "hu" works anyway
+    "yi": None,  # historical, not used (large)
+    "ye": None,  # historical, not used (large)
+    "xyi": None,  # historical, not used (small)
+    "xye": None,  # historical, not used (small)
+    "wi": "wyi",  # historical, only used in names now
+    "wu": "nn",  # "wu" is historical and not used, instead w+up mapped to "ん"
+    "we": "wye",  # historical, only used in names now
+    "va": None,  # not used as a single kana, just vu for ゔ/ヴ
+    "vi": None,  # not used as a single kana, just vu for ゔ/ヴ
+    "ve": None,  # not used as a single kana, just vu for ゔ/ヴ
+    "vo": None,  # not used as a single kana, just vu for ゔ/ヴ
+}
 
 
 def romaji_simple_mapping(
@@ -32,7 +100,7 @@ def romaji_simple_mapping(
 ) -> str:
     """Generate Karabina Elements JSON to map key+modifier to given romaji key sequence.
 
-        Using the following mappings for macOS Japanese - Romaji mode:
+    Using the following mappings for macOS Japanese - Romaji mode:
 
     * `は` from `h` alone to `ha`
     * `ひ` from `h` + `left_arrow` to `hi`
@@ -90,5 +158,14 @@ def romaji_simple_mapping(
     )
 
 
-for modifier, suffix in zip(modifiers, vowels):
-    print(romaji_simple_mapping("h", modifier, "h" + suffix))
+# a -> あ, a+left -> い, etc
+for modifier, vowel in zip(modifiers, vowels):
+    print(romaji_simple_mapping("a", modifier, vowel))
+
+for prefix in rows:
+    for modifier, suffix in zip(modifiers, vowels):
+        romaji = prefix + suffix
+        romaji = exceptions.get(romaji, romaji)  # apply exception
+        if not romaji:
+            continue  # skip the historical entries "yi" and "ye"
+        print(romaji_simple_mapping(prefix, modifier, romaji))
